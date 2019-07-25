0a239fbfa0048d00cffb7272c6160955c3c5211d
techniboy <techniboy.h2o@gmail.com>
2019-07-26 00:47:34 +0100

    add simple react app

---
+++ fe/my_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/welcome'
+
+function App() {
+  return (
+    <Welcome name='Sarah'/>
+  );
+}
+
+export default App;
